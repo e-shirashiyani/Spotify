e199274de58f0518bdc328a1e275aba0934e6df7
e.shirashiyani <e.shirashiyani>
2024-04-14 16:35:32 +0330

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by e.shirashiyani on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
